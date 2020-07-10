0b99c5f84ac204840c4c208c0b4e7b0513400919
louchenyao <louchenyao@gmail.com>
2020-07-10 16:23:22 +0000

    fix some minor compiliation errors

--- benchmarks/tpcc_txn.cpp
+++ benchmarks/tpcc_txn.cpp
@@ -208,11 +208,11 @@ RC tpcc_txn_man::run_payment(tpcc_query * query) {
 	}
 	
 	char h_data[25];
-	strncpy(h_data, w_name, 10);
+	strncpy(h_data, w_name, 11);
 	int length = strlen(h_data);
 	if (length > 10) length = 10;
 	strcpy(&h_data[length], "    ");
-	strncpy(&h_data[length + 4], d_name, 10);
+	strncpy(&h_data[length + 4], d_name, 11);
 	h_data[length+14] = '\0';
 	/*=============================================================================+
 	  EXEC SQL INSERT INTO
